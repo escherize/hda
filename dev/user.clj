3db762ee9a20fe9d035a4c2be6f702d15837c540
Bryan Maass <mbcm@Bryans-MacBook-Pro.local>
2025-04-03 21:37:01 -0600

    boop

--- dev/user.clj
+++ dev/user.clj
@@ -3,7 +3,7 @@
             [nrepl.server :as nrepl]))
 
 (defn start-nrepl-server []
-  (let [server (nrepl/start-server :port 7888)]
+  (let [server (nrepl/start-server :port 7888 :bind "0.0.0.0")]
     (println "nREPL server started on port 7888")
     server))
 
