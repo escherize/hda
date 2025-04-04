435366ff94a74f500ed20a793b3d9ef0ebaff4bc
Bryan Maass <mbcm@Bryans-MacBook-Pro.local>
2025-04-03 21:32:11 -0600

    boop

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,12 @@
 (ns user
-  (:require [myapp.core :as core]))
+  (:require [myapp.core :as core]
+            [nrepl.server :as nrepl]))
+
+(defn start-nrepl-server []
+  (let [server (nrepl/start-server :port 7888)]
+    (println "nREPL server started on port 7888")
+    server))
 
 (defn -main [& args]
+  (start-nrepl-server)
   (apply core/-main args))
