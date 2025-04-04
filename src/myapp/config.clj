10c3c9f4d45dc8a5fda7b2e9ea65162e461feac5
Bryan Maass <mbcm@Bryans-MacBook-Pro.local>
2025-04-03 21:26:08 -0600

    boop

--- src/myapp/config.clj
+++ src/myapp/config.clj
@@ -1,20 +1,29 @@
 (ns myapp.config
-  (:require [mount.core :refer [defstate]]))
+  (:require [mount.core :refer [defstate]]
+            [clj-yaml.core :as yaml]))
+
+(defn get-config []
+  (when-let [config-path (or (System/getenv "HDA_CONFIG") "hda_config.yml")]
+    (when-let [config-contents (slurp config-path)]
+      (when-let [config-map (yaml/parse-string config-contents)]
+        config-map))))
 
 (defstate config
   :start
-  {:server {:port 3333}
+  (merge
+    (get-config)
+    {:server {:port 3337}
 
-   :jdbc-url
-   "jdbc:sqlite:target/db.sqlite"
+     :jdbc-url
+     "jdbc:sqlite:target/db.sqlite"
 
-   :schema
-   {:user/screen-name       {:db/unique :db.unique/identity}
-    :user/magic-link        {}
-    :user/link-generated-at {:db/type :db.type/long}
-    :user/signup-at         {:db/type :db.type/long}
+     :schema
+     {:user/screen-name       {:db/unique :db.unique/identity}
+      :user/magic-link        {}
+      :user/link-generated-at {:db/type :db.type/long}
+      :user/signup-at         {:db/type :db.type/long}
 
-    :todo/title     {}
-    :todo/done?     {:db/type :db.type/boolean}
-    :todo/done-date {:db/type :db.type/long}
-    :todo/user      {:db/valueType :db.type/ref}}})
+      :todo/title     {}
+      :todo/done?     {:db/type :db.type/boolean}
+      :todo/done-date {:db/type :db.type/long}
+      :todo/user      {:db/valueType :db.type/ref}}}))
